0a43e88925109aee2cdb25ba1d65674713008b43
Kushal Kumar <kushi.17.1987@gmail.com>
2018-06-04 17:11:47 +0530

    import issue because renamed file name.

--- Server/app.js
+++ Server/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var errorHandler = require('./Errors/error-handler');
+var errorHandler = require('./Errors/error-hanlder');
 
 var app = express();
 
