8f6be8fe0589a255793d5111005fcad39215d988
Kushal Kumar <kushi.17.1987@gmail.com>
2018-06-13 18:03:54 +0530

    Added param router in express.

--- Server/routes/users.js
+++ Server/routes/users.js
@@ -1,9 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+router.param('id', function (req, res, next, id) {
+  console.log('CALLED ONLY ONCE');
+  next();
+});
+
+router.get('/:id', function (req, res, next) {
+  console.log('although this matches');
+  next();
+});
+
+router.get('/:id', function (req, res) {
+  console.log('and this matches too');
+  res.end();
+});
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+   console.log('/users/ Router')
+   res.send('respond with a resource');
 });
 
 module.exports = router;
