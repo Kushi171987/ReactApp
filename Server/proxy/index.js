b934d7bc9fe7b7c7e86e29dd6382abbef1b29f4f
Kushal Kumar <kushi.17.1987@gmail.com>
2018-06-21 20:03:23 +0530

    little clean up.

--- Server/proxy/index.js
+++ Server/proxy/index.js
@@ -1,16 +1,16 @@
 var Client = require('./Client').Client;
 var C = require('./lib/constants');
-var sockets = require('socket.io');
+var Socket = require('socket.io');
 
 let clients = [];
 
 function configure(server){
    console.log('configuring the proxy server');
-   var io = sockets(server);
+   var socket = Socket(server);
 
-   io.on(C.CONNECTION, function(socket) {
+   socket.on(C.CONNECTION, function(conn) {
       let id = ++C.id;
-      let client = new Client(socket, id, id);
+      let client = new Client(conn, id, id);
       clients[client.name] = client;
       console.log(client.name, ' connected');
    });
