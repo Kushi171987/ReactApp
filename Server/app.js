3e024dd0b1be8c9ebf4659743ce04dd8a711710b
Kushal Kumar <kushi.17.1987@gmail.com>
2018-06-29 18:59:58 +0530

    Added compression module

--- Server/app.js
+++ Server/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
+var compression = require('compression');
 
 var admin = require('./admin');
 var indexRouter = require('./routes/index');
@@ -15,6 +16,7 @@ var app = express();
 app.locals.title = 'AppServer';
 
 app.use(logger('dev'));
+app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
