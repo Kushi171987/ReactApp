604f1b52d9abde53beae2ee2f676b9e7f1e496df
Kushal Kumar <kushi.17.1987@gmail.com>
2018-07-02 16:24:43 +0530

    Added config options for socket.io

--- Server/proxy/index.js
+++ Server/proxy/index.js
@@ -3,9 +3,21 @@ var Socket = require('socket.io');
 var clients = require('./clients');
 var C = require('./lib/constants');
 
+var SOCKET_CONFIG = {
+   // path: '/test',
+   // adapter: 
+   // origins: *, 
+   // parser :
+   // transports ['polling', 'websocket'],
+   serveClient: false,
+   pingInterval: 20000,
+   pingTimeout: 10000,
+   cookie: false
+};
+
 function configure(server){
    console.log('configuring the proxy server');
-   var socket = Socket(server);
+   var socket = Socket(server, SOCKET_CONFIG);
 
    socket.on(C.CONNECTION, function(conn) {
       clients.addWithConnection(conn);
